69356418ef1893ba0953ecbd4a961e4f2a02b99b
kyw <rladyddn258@gmail.com>
2024-12-06 17:02:52 +0900

    test

--- MDMSender/obj/Release/net8.0-windows/MDMSender.AssemblyInfo.cs
+++ MDMSender/obj/Release/net8.0-windows/MDMSender.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MDMSender")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7535a7300722808eef9db19bf80c2c32a0aefd5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d59154d056e0e87ab7fc9918a4c9384cbaef9ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("MDMSender")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MDMSender")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
