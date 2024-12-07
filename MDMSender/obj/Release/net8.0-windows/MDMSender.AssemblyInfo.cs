7ad8b39be36eab5bf56f5a9ecf71473dd3260904
DESKTOP-551ENR9\kyw <rladyddn258@gmail.com>
2024-12-07 17:51:45 +0900

    정리

--- MDMSender/obj/Release/net8.0-windows/MDMSender.AssemblyInfo.cs
+++ MDMSender/obj/Release/net8.0-windows/MDMSender.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MDMSender")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef9a331c02d526fc6f416aa493b6b05a7d7cbda2")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42eacb8b41770f60cb100b5b440aeb6c84003e9b")]
->>>>>>> TempBranch
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54fb048c070cbc9b4222f12dd38cf04422a0f292")]
 [assembly: System.Reflection.AssemblyProductAttribute("MDMSender")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MDMSender")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
